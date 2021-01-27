41f263f27269a9cacbbc75104a47c494ae1c4089
Vladislav Korecký <vladislav.ml@korecky.org>
2021-01-27 13:44:08 +0100

    Adding variables

--- story_reader.py
+++ story_reader.py
@@ -1,9 +1,11 @@
 import yaml
 
+
 class StoryManager:
     def __init__(self):
         self.story = None
         self.frame = None
+        self.variables = {}
 
     def start(self, file_name):
         with open(file_name) as file:
@@ -18,7 +20,16 @@ class StoryManager:
 
     def load_frame(self):
         # print text
-        print(self.frame.get("text"))
+        text = self.frame.get("text")
+
+        var_start_index = None
+        for ch_index in range(len(text)):
+            if text[ch_index] == '{':
+                var_start_index = ch_index
+            elif text[ch_index] == '}':
+                text = text.replace(text[var_start_index:ch_index + 1], str(self.variables.get(text[var_start_index + 1:ch_index])))
+
+        print(text)
 
         # check if the frame is end frame
         if len(self.frame.get("options")) == 0:
@@ -33,6 +44,13 @@ class StoryManager:
         for option_index in range(len(self.frame.get("options"))):
             print(str(option_index) + " " + self.frame.get("options")[option_index])
 
-        # handle actions
+        # select action
         action = int(input())
+
+        # handle variables
+        if "variables" in self.frame:
+            var = self.frame.get("variables")
+            self.variables[var[action][0]] = var[action][1]
+
+        # set next frame
         self.frame = self.story[self.frame.get("actions")[action]]
